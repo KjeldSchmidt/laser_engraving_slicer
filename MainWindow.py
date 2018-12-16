49e1d7002fced9e51c7635405d72406ed6b1b6ef
Kjeld Schmidt <contact@kjeld-schmidt.com>
2018-12-16 01:33:32 +0100

    Added source image selection dialog.

--- MainWindow.py
+++ MainWindow.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QMainWindow, QAction
+from PyQt5.QtWidgets import QMainWindow, QAction, QFileDialog
 
 
 class MainWindow( QMainWindow ):
@@ -17,11 +17,25 @@ class MainWindow( QMainWindow ):
 
 	def make_file_menu( self ):
 		main_menu = self.menuBar()
-		file_menu = main_menu.addMenu('File')
+		file_menu = main_menu.addMenu( 'File' )
 
-		close_app = QAction('Exit', self)
-		close_app.setShortcut('Ctrl+Q')
-		close_app.setStatusTip('Exit this program')
-		close_app.triggered.connect(self.close)
-		file_menu.addAction(close_app)
+		close_app = QAction( 'Exit', self )
+		close_app.setShortcut( 'Ctrl+Q' )
+		close_app.setStatusTip( 'Exit this program' )
+		close_app.triggered.connect( self.close )
 
+		open_file = QAction( 'Open File', self )
+		open_file.setStatusTip( 'Open a source file for slicing' )
+		open_file.triggered.connect( self.open_source_file )
+
+		file_menu.addAction( open_file )
+		file_menu.addAction( close_app )
+
+	def open_source_file( self ):
+		picked_file_name = QFileDialog.getOpenFileName( self, 'Open file',
+			filter="All Graphics (*.png *.bmp *.jpeg *.jpg *.svg);;Pixel Graphics (*.png *.bmp *.jpeg *.jpg);;Vector Graphics (*.svg)" )
+
+		if picked_file_name[ 0 ]:
+			with open( picked_file_name[ 0 ], 'rb' ) as source_file:
+				data = source_file.read()
+				print( data )
